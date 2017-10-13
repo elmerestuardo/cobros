5eece83c024bab25181f79aa5ddd56863c92507c
Elmer pedroza <elmerpe@mapfre.com.gt>
2017-10-13 17:04:03 -0600

    Mejore el login con materialize

--- login.php
+++ login.php
@@ -4,163 +4,66 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Prestamos Emmanuel</title>
-        <link href="Content/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <style type="text/css"> 
-            footer{
-                position: absolute;
-  bottom: 0;
-  width: 100%;
-  height: 0px;
-  
-            }
-            body{
-                margin-bottom: 0px;
-            }
-            /*FOOTER 1*/
-.footer1 a {
-    color: #DDD;
-}
-
-.footer1 a:hover,
-.footer1 a:focus,
-.footer1 a:active {
-    color: #FFF;
-}
-
-.footer1 .footer {
-    background-color: #2E3436;
-    padding: 35px 0;
-    margin-top: 5em;
-}
-
-.footer1 .footer .footer-desc p {
-    color: #DDD;
-	margin: 15px 0 8px;
-}
-
-.footer1 .footer .social {
-    margin: 0 auto;
-    display: table;
-}
-
-.footer1 .footer .social li {
-    float: left;
-}
-
-.footer1 .footer .social li a {
-	display: block;
-    font-size: 1em;
-    color: #DDD;
-    width: 30px;
-    height: 30px;
-    line-height: 32px;
-    text-align: center;
-    background-color: #222728;
-    border-radius: 2px;
-    margin: 15px 4px
-}
-
-.footer1 .footer .social li a:hover {
-    background-color: #3A4144;
-}
-
-.footer1 .footer .input-group {
-    margin: 15px 0;
-}
-
-.footer1 .footer .form-control {
-    border-radius: 2px 0 0 2px;
-    background-color: #585C5D;
-    border-color: #585C5D;
-    color: #DDD;
-    font-size: 15px;
-}
-
-.footer1 .footer .form-control:hover,
-.footer1 .footer .form-control:focus {
-    border: 1px solid #222728;
-    background-color: #222728;
-}
-
-.footer1 .footer .input-group-addon {
-    border-radius: 0 2px 2px 0;
-    background-color: #F7931D;
-    color: #FFF;
-    border-color: #F7931D;
-    font-size: 15px;
-}
-
-.footer1 .footer-bottom {
-    background-color: #222728;
-    padding: 15px 0;
-    color: #DDD;
-    font-size: 13px;
-}
-
-.footer1 .footer-bottom ul li {
-    display: inline-block;
-}
-
-.footer1 .footer-bottom ul li a {
-    display: block;
-    padding-left: 8px;
-}
-
-.footer1 .footer-bottom ul li a:after {
-    content: "-";
-    padding-left: 8px;
-}
-
-.footer1 .footer-bottom ul li:last-child a:after {
-    content: "";
-}
-
-@media (max-width: 550px) {
-	.footer1 .footer-bottom .pull-left,
-	.footer1 .footer-bottom .pull-right {
-		float: none !important;
-		width: 100%;
-		padding: 10px;
-		text-align: center;
-	}
-}
-        </style>
+        <!--<link href="Content/bootstrap.min.css" rel="stylesheet" type="text/css"/>-->
+        <link href="materialize/css/materialize.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
         <div class="container">
             <form method="post" action="../prestamos/DAL/login.php">
-                <div class="col-md-12 col-xs-12">
-                    <div class="col-md-4 col-md-offset-4 col-xs-12 col-xs-offset-0">
-                    <div class="col-md-12 col-xs-12">
+                <div class="row">
+                <div class="col s12">
+                    <div class="col s4 offset-s4">
+                    <div class="col s12">
                         <p>Por favor ingrese sus datos para iniciar sesion</p>
                     </div>
-                    <div class="form-group col-md-12 col-xs-12">
-                        <label class="control-label">Usuario:</label>
-                        <input type="text" name="usuario" class="form-control">
+                    <div class="input-field col s12">
+                        <i class="material-icons prefix">account_circle</i>                        
+                        <input type="text" name="usuario" class="validate">
+                        <label for="usuario">Usuario</label>
                     </div>
-                    <div class="form-group col-md-12 col-xs-12">
-                        <label class="control-label">Contraseña:</label>
-                        <input type="text" name="password" class="form-control">
+                    <div class="input-field col s12">
+                        <i class="material-icons prefix">phone</i>
+                        <input type="text" name="password" class="validate">
+                        <label for="password">Contraseña</label>
                     </div>
-                    <div class="col-md-8 col-md-offset-2 col-xs-12 col-xs-offset-0">
-                        <button type="submit" class="btn btn-primary btn-block">Iniciar</button>
+                    <div class="col s8 offset-s2">
+                        <button type="submit" class="btn waves-effect waves-light">Iniciar
+                        <i class="material-icons right">send</i></button>
                     </div>
                 </div>
                 <!--<button type="button" id="btnPrueba" class="btn btn-danger">Hola a todos</button>-->
             </div>
+                </div>
                 </form>            
         </div>
-        <!--FOOTER 1-->
-<footer class="footer1">
-    
-    
-    <div class="footer-bottom">
-        <div class="container">
-            <div class="text-center"> Copyright © <a href="">Rizwan Akram</a>.  All right reserved.</div>
-        </div>
-    </div> <!--/.footer-bottom--> 
-</footer>
+        
+<footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2014 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
         <script src="Scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
-        <script src="Scripts/bootstrap.min.js" type="text/javascript"></script>
+        <!--<script src="Scripts/bootstrap.min.js" type="text/javascript"></script>-->
+        <script src="materialize/js/materialize.js" type="text/javascript"></script>
     </body>   
 </html>
